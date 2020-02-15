470c9ced57a1eb13b081d5ce055e4993a80760fc
caub <cyril.auburtin@gmail.com>
2020-02-15 13:23:21 +0100

    add examples/simple build for GH pages

---
+++ examples/simple/build/precache-manifest.b832cb03c7475bbaef62186fcaea04c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52019e479085fb7f8c2eccc981e1f6fe",
+    "url": "/wurd-react/examples/simple/build/index.html"
+  },
+  {
+    "revision": "1b5a22dd74cdd317e939",
+    "url": "/wurd-react/examples/simple/build/static/js/2.a40b7440.chunk.js"
+  },
+  {
+    "revision": "c08de396b159883c04ab4c65f1180e2a",
+    "url": "/wurd-react/examples/simple/build/static/js/2.a40b7440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd452d80ee06771cb6d",
+    "url": "/wurd-react/examples/simple/build/static/js/main.0a68ea9f.chunk.js"
+  },
+  {
+    "revision": "2acdde60a2df936d63a9e6f11483777a",
+    "url": "/wurd-react/examples/simple/build/static/js/main.0a68ea9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfbc2763a2adebb17936",
+    "url": "/wurd-react/examples/simple/build/static/js/runtime-main.78b421aa.js"
+  }
+]);
